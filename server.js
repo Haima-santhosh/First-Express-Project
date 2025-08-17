6ac6c06c283f4069f4f1c33fad767c299e9b8cca
Haima-santhosh <hymasanthosh14@gmail.com>
2025-08-17 22:51:11 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+app.get('/',(req,res)=>
+{
+    res.send("Hello World !!!!!!!!!!!!!!!")
+})
+
+app.get('/about',(req,res)=>
+{
+    res.send("This is about page !!!!!!!!!!!!!!!")
+})
+app.get('/contact',(req,res)=>
+{
+    res.send("This is contact page !!!!!!!!!!!!!!!")
+})
+
+const PORT = 3000;
+app.listen(PORT,()=>
+{
+    console.log(`Server is running on http://localhost:${PORT}`);
+    
+})
